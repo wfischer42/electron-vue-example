4f62cc749c135f8db1cf5ce9e8a8a42a18b9285e
William Fischer <william.fischer42@gmail.com>
2019-03-04 15:57:56 -0700

    WIP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello h-screen w-screen bg-red">
-
     <h1>{{ msg }}</h1>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">Cras justo odio</li>
